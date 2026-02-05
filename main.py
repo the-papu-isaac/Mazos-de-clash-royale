faa640d0067efce30402f29effe86ea1c5e31195
the papu isaac <Dinoisaac0915@gmail.com>
2026-02-05 14:29:33 -0500

    mi primer commit agrege mi primer mazo

---
+++ main.py
@@ -0,0 +1,12 @@
+deck_1 = {
+    1: "bruja",
+    2: "bola de fuego",
+    3: "ariete de batalla", 
+    4: "caballero",
+    5: "bola de nieve",
+    6: "lapida",
+    7: "guardias",
+    8: "duendes con lanza",
+}
+
+print(deck_1[4])
